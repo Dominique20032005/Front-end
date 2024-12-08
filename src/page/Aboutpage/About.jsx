b5e6e257a2e72ae6c8bd78b3035d486f26b12a71
nhad1702 <anhducnguyenhuu2@gmail.com>
2024-12-08 11:48:03 +0700

    ahihi

--- src/page/Aboutpage/About.jsx
+++ src/page/Aboutpage/About.jsx
@@ -10,26 +10,26 @@ const About = () => {
         <h1>UIT-Learning</h1>
         <p>Created and designed on Dec 7th 2024, UIT-Learning is the place where you can share and explore.</p>
         <p>We hope that UIT-Learning can help you with your education process, meet new people, learning new things that you wanted.</p>
-        <div className="stat">
-          <div className='stats'>
+        <div className="stat w-fit flex justify-evenly">
+          <div className='stats flex items-center justify-center flex-col'>
             <h2>2</h2>
             <p>Creators</p>
           </div>
-          <div className='stats'>
+          <div className='stats flex items-center justify-center flex-col'>
             <h2>20</h2>
             <p>Members</p>
           </div>
-          <div className='stats'>
+          <div className='stats flex items-center justify-center flex-col'>
             <h2>10</h2>
             <p>Posts</p>
           </div>
         </div>
-        <div className="creator">
-          <div className="person">
+        <div className="creator w-fit flex items-center justify-evenly">
+          <div className="person flex items-center justify-center flex-col">
             <h1>Nguyen Huu Anh Duc</h1>
             <h3>ID: 23560009</h3>
           </div>
-          <div className="person">
+          <div className="person flex items-center justify-center flex-col">
             <h1>Ho Huy Thien</h1>
             <h3>ID: 23560063</h3>
           </div>
