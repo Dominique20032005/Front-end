ebadfe96d41b6f76c1277efb1d9cb2ad1a37827f
nhad1702 <anhducnguyenhuu2@gmail.com>
2024-12-28 15:52:23 +0700

    ahihi

--- src/page/Main/Main.jsx
+++ src/page/Main/Main.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import './Main.css'
+import '../../assets/font/Oswald/Oswald.css'
 
 const Main = () => {
   return (
     <>
       <Header></Header>
       <div className='board w-full h-screen flex items-center justify-center'>
-        <h1>Welcome to UIT-Learning. Join with us for more interesting content</h1>
+        <h1 className='poppins-regular'>Welcome to UIT-Learning. Join with us for more interesting content</h1>
         <div className='new-Post flex items-center flex-col'>
           <h3>New post</h3>
           <div className='post'></div>
