bdb7ac92ddc63257038981085850a12ce2303fa6
nhad1702 <anhducnguyenhuu2@gmail.com>
2025-01-15 23:32:19 +0700

    carousel add

--- src/page/Main/Main.jsx
+++ src/page/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import CarouselItem from '../../components/Carousel'
 import './Main.css'
 import '../../assets/font/Oswald/Oswald.css'
 
@@ -13,6 +14,9 @@ const Main = () => {
         <div className="left">
           <h1 className='oswald-regular text-4xl'>Welcome to Learning-Social. Join with us for more interesting content</h1>
           <h2 className='text-2xl'>For more post? <Link to='/post' className='font-bold text-5xl cursor-pointer'>Click here</Link></h2>
+        </div>
+        <div className="right">
+        <CarouselItem autoplay={true} interval={5000} fade={true} />
         </div>
       </div>
       <Footer></Footer>
